5e0109a818bb438c0162af80924756a9e64c2247
neekolaytoledo <60115771+neekolaytoledo@users.noreply.github.com>
2020-01-27 11:08:59 -0500

    Add files via upload

---
+++ Desafio143.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DESAFIO143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese w: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese D: ");
+            double d = double.Parse(Console.ReadLine());
+
+            double drad = d * (Math.PI / 180);
+            double y = (Math.Cos(drad) * w) - x;
+
+            Console.WriteLine("D=" + d + "°" + " x=" + x + " w=" + w + " y=" + y);
+
+        }
+    }
+}
