1b2e8e2956b34127f557529e5a024b27cee89d80
neekolaytoledo <60115771+neekolaytoledo@users.noreply.github.com>
2020-02-24 15:21:18 -0500

    Add files via upload

---
+++ Euler_CiclosFor(Clase)_11.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclosFor_Clase__11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de X.");
+            double X = double.Parse(Console.ReadLine());
+            double eulerX = 0;
+
+            for(int i = 0; i < 100; i++)
+            {
+                double euler;
+                euler = (Math.Pow(X,i)) / (Factorial(i));
+                eulerX += euler;
+            }
+            Console.WriteLine("e^" + X + "=" + eulerX);
+
+        }
+
+        public static double Factorial(double number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
---
+++ Sin_CiclosFor(Clase)_11.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_CiclosFor_Clase__11
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Ingrese el valor de X.");
+                double X = double.Parse(Console.ReadLine());
+                double sinX = 0;
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    double sin;
+                    sin = ((Math.Pow(-1, i)) / (Factorial(2*(i)+1)))*(Math.Pow(X,(2*i)+1));
+                    sinX += sin;
+                }
+                Console.WriteLine("sin(" + X + ")=" + sinX);
+
+            }
+
+            public static double Factorial(double number)
+            {
+                if (number == 0)
+                {
+                    return 1;
+                }
+                if (number == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return number * Factorial(number - 1);
+                }
+            }
+        }
+    
+}
